e69c97d6bb44bf7e907b01db0d58f8d1d844d195
Shivam Paliwal <shivam.p@ascentbusiness.com>
2023-01-17 12:09:52 +0530

    Diff Java file added

---
+++ My-Git-App/Diff.java
@@ -0,0 +1,13 @@
+public class Diff {
+    public static void main(String[] args) {
+        
+        int d = 10;
+        int e = 20;
+
+        int f = d - e;
+
+        System.out.println(f);
+
+    }
+    
+}
