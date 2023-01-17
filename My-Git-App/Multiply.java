d54d2080886ef3207a2321a58daa3dd2cf28be2c
Mohd Faizan <faizan.m@ascentbusiness.com>
2023-01-17 11:52:54 +0530

    developed

---
+++ My-Git-App/Multiply.java
@@ -0,0 +1,14 @@
+
+public class Multiply {
+    public static void main(String[] args) {
+        
+        int d = 10;
+        int e = 20;
+
+        int f = d + e;
+
+        System.out.println(f);
+
+    }
+    
+}
